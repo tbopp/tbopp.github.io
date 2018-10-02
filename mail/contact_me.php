043cefedeb5858b7308cfe5c1ea4fd60701613d0
byt3_h34d <mr.tybopp@gmail.com>
2018-10-02 00:46:19 -0500

    updating form logic, testing formspree.com

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
@@ -19,4 +19,4 @@ $header .= "Reply-To: $email";
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
-?>
+?> -->
